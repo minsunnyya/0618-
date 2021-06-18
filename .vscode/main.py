fb46525fb34ac141711f8dd0580a4141c34b7fc6
username <sun2547@naver.com>
2021-06-18 13:43:10 +0900

    Create main.py

---
+++ .vscode/main.py
@@ -0,0 +1,32 @@
+#화면에  Hello World 문자열을 출력하시오.
+print("Hello World")
+
+#자신의 성명과 전화번호 , Email을 출력하시오.
+print("이름 : 김민선")
+print("전화번호 : 010-6471-4871")
+print("Email : sun2547@naver.com")
+
+#화면에 Mary's cosmetics를 출력하시오.
+print(" Mary's cosmetics")
+
+#"홍길동"의 문자열을 변수 name에 저장한 후 출력하시오.
+name = "홍길동"
+print(name)
+
+#다음 변수에 있는 문자열을 출력결과에 맞게 출력하시오.
+
+#input 사용하기
+sunny = input()
+
+#int로 더하기
+
+a = int(input("숫자 "))
+b = int(input("숫자 "))
+print("합 :", (a+b))
+
+c= int(input("숫자"))
+d= int(input("숫자"))
+f= int(input("숫자"))
+print("합은?", (a+b))
+
+
